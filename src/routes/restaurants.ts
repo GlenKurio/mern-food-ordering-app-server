6c2a3ebbfdd2f92c9464e73c885963daa759eed9
Oleh Minko <gleb.kurip@gmail.com>
2024-08-02 17:23:16 -0500

    manage restaurant api. Cloudinary not working

--- src/routes/restaurants.ts
+++ src/routes/restaurants.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import multer from "multer";
 import restaurantsContoller from "../controllers/restaurants";
+import { validateMyRestaurantRequest } from "../middleware/validation";
+import { jwtCheck, jwtParse } from "../middleware/auth";
 const router = express.Router();
 
 const storage = multer.memoryStorage();
@@ -9,6 +11,9 @@ const upload = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } });
 router.post(
   "/",
   upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
   restaurantsContoller.createRestaurant
 );
 
